299d3942e20e20445c7419f9db039b03a7b94dcc
Den <53218277+PhantomOfLight@users.noreply.github.com>
2019-10-26 22:26:34 +0300

    Update model.py

--- efficientnet_pytorch/model.py
+++ efficientnet_pytorch/model.py
@@ -206,9 +206,9 @@ class EfficientNet(nn.Module):
         return cls(blocks_args, global_params)
 
     @classmethod
-    def from_pretrained(cls, model_name, num_classes=1000, in_channels = 3):
+    def from_pretrained(cls, model_name, num_classes=6, in_channels = 3):
         model = cls.from_name(model_name, override_params={'num_classes': num_classes})
-        load_pretrained_weights(model, model_name, load_fc=(num_classes == 1000))
+        load_pretrained_weights(model, model_name, load_fc=(num_classes == 6))
         if in_channels != 3:
             Conv2d = get_same_padding_conv2d(image_size = model._global_params.image_size)
             out_channels = round_filters(32, model._global_params)
@@ -216,9 +216,9 @@ class EfficientNet(nn.Module):
         return model
     
     @classmethod
-    def from_pretrained(cls, model_name, num_classes=1000):
+    def from_pretrained(cls, model_name, num_classes=6):
         model = cls.from_name(model_name, override_params={'num_classes': num_classes})
-        load_pretrained_weights(model, model_name, load_fc=(num_classes == 1000))
+        load_pretrained_weights(model, model_name, load_fc=(num_classes == 6))
 
         return model
 
